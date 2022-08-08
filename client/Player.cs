9400e7a4fcece5b5fae7b4726d65bbcbf0c79abf
Asokanta <asokanta0@gmail.com>
2022-08-08 20:12:24 +0300

    Add files via upload

---
+++ client/Player.cs
@@ -0,0 +1,32 @@
+﻿namespace client
+{
+    internal class Player
+    {
+        // these are accessable ones from client
+        private int id = 0;
+        public int hp = 100;
+        public string nickname = "New_Player";
+
+        public float posX = 0.0f;
+        public float posY = 0.0f;
+        public float posZ = 0.0f;
+        public float rot = 0.0f;
+        public int ping = 0;
+
+
+        public void initPlayer(int plyId)
+        {
+
+            id = plyId;
+
+        }
+
+        public int getPlayerID()
+        {
+
+            return this.id;
+
+        }
+
+    }
+}
